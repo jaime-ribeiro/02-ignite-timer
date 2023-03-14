ce8c54ed39db36a0af3d18345c8a7358e0be392e
jaime-ribeiro <littlejou@gmail.com>
2023-03-14 18:35:43 -0300

    History Page :lipstick:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { HeaderContainer } from './styles'
-import { logoIgnite } from '../../assets/logo-ignite.svg'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logoIgnite} alt="" />
       <nav>
-        <a href="teste">timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
